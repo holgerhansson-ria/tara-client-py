c57450c70a6fe894e98b1fd52b41814471c1c35e
traadipundar <hansson.holger@gmail.com>
2017-10-29 20:29:33 +0200

    Initial

--- client/clientconf.py
+++ client/clientconf.py
@@ -3,7 +3,7 @@
 defaultAuthUrl = 'https://tara-test.ria.ee/oidc/authorize'
 defaultScope = 'openid'
 defaultResponse_type = 'code'
-defaultRedirect_uri = "https://tara-client-py.herokuapp.com"
+defaultRedirect_uri = "https://tara-client-py.herokuapp.com/response"
 defaultClientId = 'openIdDemo2'
 defaultSecret = 'secret'
 defaultState = 'hkMVY7vjuN7xyLs5'
